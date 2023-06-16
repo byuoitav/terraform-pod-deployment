88114e9e416ab6a3e3ce8637c1639aef6401473c
Clinton Reeder <clinton_reeder@byu.edu>
2023-06-16 12:42:17 -0600

    Added locals to fix load balancer issue

--- modules/kubernetes-deployment/main.tf
+++ modules/kubernetes-deployment/main.tf
@@ -31,18 +31,16 @@ data "aws_lb" "eks_lb_private" {
   }
 }
 
+locals {
+  load_balancer = var.private ? data.aws_lb.eks_lb_private.id : data.aws_lb.eks_lb_public.id
+}
+
 # Defining this variable here to keep the variable decision for load balancer with the line for 
 # determining the load balancer information based on cluster information
 variable "load_balancer" {
-  type    = string
+  type        = string
   description = "Variable that determines which type of load balancer is in play and to use that load balancer for deployment"
-  
-  dynamic "default" { 
-    for_each = var.private ? [data.aws_lb.eks_lb_private.value] : [data.aws_lb.eks_lb_public.value]
-    content {
-      value = default.value
-    }
-  }
+  default     = local.load_balancer
 }
 
 /*
